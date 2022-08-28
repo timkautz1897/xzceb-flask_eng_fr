dfd27dd29e3bee61b94ac9f126043e6cd3926a00
Tim Kautz <tim-kautz@gmx.de>
2022-08-28 19:39:54 +0000

    Tests for translator

---
+++ final_project/machinetranslation/tests/test_translator.py
@@ -0,0 +1,20 @@
+import unittest
+
+from .. import translator
+
+class TestTranslation(unittest.TestCase):
+
+    def test_f2e_translates(self):
+        self.assertEqual(translator.french_to_english('Bonjour'), 'Hello')
+
+    def test_f2e_empty_text(self):
+        self.assertEqual(translator.french_to_english(''), '')
+
+    def test_e2f_translates(self):
+        self.assertEqual(translator.english_to_french('Hello'), 'Bonjour')
+
+    def test_e2f_empty_text(self):
+        self.assertEqual(translator.english_to_french(''), '')
+
+if __name__ == '__main__':
+    unittest.main()
